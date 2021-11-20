014b52853dcf802224ff0975cb2df68e82a08d2c
EnioAmarantes <enio@forlogic.org>
2021-11-20 19:42:03 -0300

    Iniciando o projeto da conexão das aplicações

---
+++ src/main/java/com/soilcorrections/SoilCorrectionAPI/SoilCorrectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.soilcorrections.SoilCorrectionAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoilCorrectionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoilCorrectionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soilcorrections/SoilCorrectionAPI/SoilCorrectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soilcorrections.SoilCorrectionAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoilCorrectionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
